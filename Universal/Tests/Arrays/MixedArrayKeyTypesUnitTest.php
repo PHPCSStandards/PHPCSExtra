badd720042f14803bcd88312ffe3c1697a6fab63
jrfnl <jrfnl@users.noreply.github.com>
2020-01-23 08:33:07 +0100

    :sparkles: New `Universal.Arrays.MixedArrayKeyTypes` sniff
    Best practice sniff: either have all array items with numeric keys or with string keys. Don't use a mix of integer and numeric  keys for array items.
    
    Includes unit tests.
    Includes documentation.

---
+++ Universal/Tests/Arrays/MixedArrayKeyTypesUnitTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * PHPCSExtra, a collection of sniffs and standards for use with PHP_CodeSniffer.
+ *
+ * @package   PHPCSExtra
+ * @copyright 2020 PHPCSExtra Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSExtra
+ */
+
+namespace PHPCSExtra\Universal\Tests\Arrays;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the MixedArrayKeyTypes sniff.
+ *
+ * @covers PHPCSExtra\Universal\Sniffs\Arrays\MixedArrayKeyTypesSniff
+ *
+ * @since 1.0.0
+ */
+class MixedArrayKeyTypesUnitTest extends AbstractSniffUnitTest
+{
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * @return array <int line number> => <int number of errors>
+     */
+    public function getErrorList()
+    {
+        return [
+            34 => 1,
+            41 => 1,
+            47 => 1,
+        ];
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * @return array <int line number> => <int number of warnings>
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
