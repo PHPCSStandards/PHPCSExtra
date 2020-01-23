8d5bb109cf4ade0507486813bb9ac4b94a381777
jrfnl <jrfnl@users.noreply.github.com>
2020-01-23 08:00:18 +0100

    :sparkles: New `Universal.UseStatements.DisallowUseClass` sniff
    Sniff to forbid using import use statements for classes/traits/interfaces.
    
    The sniff contains two error codes `FoundWithoutAlias` and `FoundWithAlias` to allow for only forbidding class `use` import statements with or without alias.
    
    Includes unit tests.
    Includes documentation.
    Includes metrics.

---
+++ Universal/Sniffs/UseStatements/DisallowUseClassSniff.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * PHPCSExtra, a collection of sniffs and standards for use with PHP_CodeSniffer.
+ *
+ * @package   PHPCSExtra
+ * @copyright 2020 PHPCSExtra Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSExtra
+ */
+
+namespace PHPCSExtra\Universal\Sniffs\UseStatements;
+
+use PHP_CodeSniffer\Exceptions\RuntimeException;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Util\Tokens;
+use PHPCSUtils\Utils\UseStatements;
+
+/**
+ * Disallow class/trait/interface import `use` statements.
+ *
+ * Related sniffs:
+ * - `Universal.UseStatements.DisallowUseFunction`
+ * - `Universal.UseStatements.DisallowUseConst`
+ *
+ * @since 1.0.0
+ */
+class DisallowUseClassSniff implements Sniff
+{
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @since 1.0.0
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return [\T_USE];
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @since 1.0.0
+     *
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
+     * @param int                         $stackPtr  The position of the current token
+     *                                               in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        try {
+            $statements = UseStatements::splitImportUseStatement($phpcsFile, $stackPtr);
+        } catch (RuntimeException $e) {
+            // Not an import use statement. Bow out.
+            return;
+        }
+
+        if (empty($statements['name'])) {
+            // No class/trait/interface import statements found.
+            return;
+        }
+
+        $tokens         = $phpcsFile->getTokens();
+        $endOfStatement = $phpcsFile->findNext([\T_SEMICOLON, \T_CLOSE_TAG], ($stackPtr + 1));
+
+        foreach ($statements['name'] as $alias => $fullName) {
+            $reportPtr = $stackPtr;
+            do {
+                $reportPtr = $phpcsFile->findNext(\T_STRING, ($reportPtr + 1), $endOfStatement, false, $alias);
+                if ($reportPtr === false) {
+                    // Shouldn't be possible.
+                    continue 2;
+                }
+
+                $next = $phpcsFile->findNext(Tokens::$emptyTokens, ($reportPtr + 1), $endOfStatement, true);
+                if ($next !== false && $tokens[$next]['code'] === \T_NS_SEPARATOR) {
+                    // Namespace level with same name. Continue searching
+                    continue;
+                }
+
+                break;
+            } while (true);
+
+            $error  = 'Use import statements for classes/traits/interfaces are not allowed.';
+            $error .= ' Found import statement for: "%s"';
+            $data   = [$fullName, $alias];
+
+            $offsetFromEnd = (\strlen($alias) + 1);
+            if (\substr($fullName, -$offsetFromEnd) === '\\' . $alias) {
+                $phpcsFile->recordMetric($reportPtr, 'Use import statement for class/interface/trait', 'without alias');
+
+                $phpcsFile->addError($error, $reportPtr, 'FoundWithoutAlias', $data);
+                continue;
+            }
+
+            $phpcsFile->recordMetric($reportPtr, 'Use import statement for class/interface/trait', 'with alias');
+
+            $error .= ' with alias: "%s"';
+            $phpcsFile->addError($error, $reportPtr, 'FoundWithAlias', $data);
+        }
+    }
+}
