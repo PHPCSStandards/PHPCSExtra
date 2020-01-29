037d8193393baee50585a89dc81b1bdf064ac927
jrfnl <jrfnl@users.noreply.github.com>
2020-01-29 17:01:15 +0100

    :sparkles: New `Universal.ControlStructures.DisallowAlternativeSyntax` sniff
    New sniff to disallow using the alternative syntax for control structures.
    
    Notes:
    * This sniff contains a public `allowWithInlineHTML` property to allow alternative syntax when inline HTML is used within the control structure. In all other cases, the use of the alternative syntax will still be disallowed.
    * The sniff has modular error codes to allow for making exceptions based on specific control structures and/or specific control structures in combination with inline HTML.
    
    Refs:
    * https://www.php.net/manual/en/control-structures.alternative-syntax.php
    * squizlabs/PHP_CodeSniffer 2842 - feature request by jrchamp
    
    Includes fixer.
    Includes unit tests.
    Includes documentation.
    Includes metrics.

---
+++ Universal/Tests/ControlStructures/DisallowAlternativeSyntaxUnitTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * PHPCSExtra, a collection of sniffs and standards for use with PHP_CodeSniffer.
+ *
+ * @package   PHPCSExtra
+ * @copyright 2020 PHPCSExtra Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSExtra
+ */
+
+namespace PHPCSExtra\Universal\Tests\ControlStructures;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the DisallowAlternativeSyntax sniff.
+ *
+ * @covers PHPCSExtra\Universal\Sniffs\ControlStructures\DisallowAlternativeSyntaxSniff
+ *
+ * @since 1.0.0
+ */
+class DisallowAlternativeSyntaxUnitTest extends AbstractSniffUnitTest
+{
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * @return array <int line number> => <int number of errors>
+     */
+    public function getErrorList()
+    {
+        return [
+            73  => 1,
+            75  => 1,
+            77  => 1,
+            82  => 1,
+            86  => 1,
+            90  => 1,
+            94  => 1,
+            103 => 1,
+            111 => 1,
+            113 => 1,
+            115 => 1,
+            119 => 1,
+            123 => 1,
+            127 => 1,
+            131 => 1,
+            138 => 1,
+        ];
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * @return array <int line number> => <int number of warnings>
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
