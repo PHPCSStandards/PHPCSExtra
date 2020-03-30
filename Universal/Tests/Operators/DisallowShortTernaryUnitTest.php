70ffed9b8d986e3950b91dfe6640205c36979ee6
jrfnl <jrfnl@users.noreply.github.com>
2020-03-31 01:40:27 +0200

    :sparkles: New `Universal.Operators.DisallowShortTernary` sniff
    New sniff to disallow using short ternaries.
    
    While short ternaries are useful when used correctly, the principle of them is often misunderstood and they are more often than not used incorrectly, leading to hard to debug issues and/or PHP warnings/notices.
    
    Includes unit tests.
    Includes documentation.
    Includes metrics.

---
+++ Universal/Tests/Operators/DisallowShortTernaryUnitTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * PHPCSExtra, a collection of sniffs and standards for use with PHP_CodeSniffer.
+ *
+ * @package   PHPCSExtra
+ * @copyright 2020 PHPCSExtra Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSExtra
+ */
+
+namespace PHPCSExtra\Universal\Tests\Operators;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the DisallowShortTernary sniff.
+ *
+ * @covers PHPCSExtra\Universal\Sniffs\Operators\DisallowShortTernarySniff
+ *
+ * @since 1.0.0
+ */
+class DisallowShortTernaryUnitTest extends AbstractSniffUnitTest
+{
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        return [
+            5 => 1,
+            6 => 2,
+            7 => 1,
+        ];
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
