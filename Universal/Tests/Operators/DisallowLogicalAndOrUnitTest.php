9c5441f2b28e356b0fbfc2ea11a3ea9d72451938
jrfnl <jrfnl@users.noreply.github.com>
2020-04-16 19:07:59 +0200

    :sparkles: New `Universal.Operators.DisallowLogicalAndOr` sniff
    New sniff to enforce the use of the boolean `&&` and `||` operators instead of the logical `and`/`or` operators.
    
    Note: as the [operator precedence](https://www.php.net/manual/en/language.operators.precedence.php) of the logical operators is significantly lower than the operator precedence of boolean operators, this sniff does not contain an auto-fixer.
    
    Includes unit tests.
    Includes documentation.
    Includes metrics.

---
+++ Universal/Tests/Operators/DisallowLogicalAndOrUnitTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * PHPCSExtra, a collection of sniffs and standards for use with PHP_CodeSniffer.
+ *
+ * @package   PHPCSExtra
+ * @copyright 2020 PHPCSExtra Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSExtra
+ */
+
+namespace PHPCSExtra\Universal\Tests\Operators;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the DisallowLogicalAndOr sniff.
+ *
+ * @covers PHPCSExtra\Universal\Sniffs\Operators\DisallowLogicalAndOrSniff
+ *
+ * @since 1.0.0
+ */
+class DisallowLogicalAndOrUnitTest extends AbstractSniffUnitTest
+{
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        return [
+            15 => 1,
+            17 => 1,
+        ];
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
