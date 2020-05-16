c47150b4e0881253ac505978c4fe8fc3f8727818
jrfnl <jrfnl@users.noreply.github.com>
2020-05-17 01:20:35 +0200

    :sparkles: New `Universal.UseStatements.LowercaseFunctionConst` sniff
    New sniff to verify that `function` and `const` keywords when used in an import `use` statement are always lowercase.
    
    Companion sniff to the upstream `Generic.PHP.LowerCaseKeyword` sniff which doesn't cover these keywords as they are not tokenized as `T_FUNCTION`/`T_CONST`, but as `T_STRING`.
    
    Includes fixer.
    Includes unit tests.
    Includes documentation.
    Includes metrics.
    
    Related:
    * squizlabs/PHP_CodeSniffer 2963

---
+++ Universal/Tests/UseStatements/LowercaseFunctionConstUnitTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * PHPCSExtra, a collection of sniffs and standards for use with PHP_CodeSniffer.
+ *
+ * @package   PHPCSExtra
+ * @copyright 2020 PHPCSExtra Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSExtra
+ */
+
+namespace PHPCSExtra\Universal\Tests\UseStatements;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the LowercaseFunctionConst sniff.
+ *
+ * @covers PHPCSExtra\Universal\Sniffs\UseStatements\LowercaseFunctionConstSniff
+ *
+ * @since 1.0.0
+ */
+class LowercaseFunctionConstUnitTest extends AbstractSniffUnitTest
+{
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        return [
+            9  => 1,
+            10 => 1,
+            13 => 1,
+            14 => 1,
+            17 => 1,
+            19 => 1,
+        ];
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
