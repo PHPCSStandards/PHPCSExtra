1ddfaa6524eb0075df8b000dac1ffd587a54c5e9
Denis Žoljom <dingo-d@users.noreply.github.com>
2022-09-25 11:48:23 +0200

    Update test file

--- Universal/Tests/DeclareStatements/BlockModeUnitTest.php
+++ Universal/Tests/DeclareStatements/BlockModeUnitTest.php
@@ -15,11 +15,11 @@ use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
 /**
  * Unit test class for the DeclareStatementsStyleUnitTest sniff.
  *
- * @covers PHPCSExtra\Universal\Sniffs\DeclareStatements\DeclareStatementsStyleSniff
+ * @covers PHPCSExtra\Universal\Sniffs\DeclareStatements\BlockModeSniff
  *
  * @since 1.0.0
  */
-class DeclareStatementsStyleUnitTest extends AbstractSniffUnitTest
+class BlockModeUnitTest extends AbstractSniffUnitTest
 {
 
     /**
@@ -32,24 +32,80 @@ class DeclareStatementsStyleUnitTest extends AbstractSniffUnitTest
     public function getErrorList($testFile = '')
     {
         switch ($testFile) {
-            case 'DeclareStatementsStyleUnitTest.1.inc':
+            case 'BlockModeUnitTest.1.inc':
                 return [
+                    21 => 1,
+                    25 => 1,
+                    29 => 1,
+                    38 => 1,
+                    47 => 1,
+                    51 => 1,
+                    55 => 1,
+                    56 => 1,
+                    60 => 1,
+                    69 => 1,
                 ];
 
-            case 'DeclareStatementsStyleUnitTest.2.inc':
+            case 'BlockModeUnitTest.3.inc':
                 return [
+                    7  => 1,
+                    11 => 1,
+                    19 => 1,
+                    20 => 1,
+                    31 => 1,
+                    38 => 1,
+                    42 => 1,
+                    43 => 1,
                 ];
 
-            case 'DeclareStatementsStyleUnitTest.3.inc':
+            case 'BlockModeUnitTest.4.inc':
                 return [
+                    27  => 1,
+                    31  => 1,
+                    56  => 1,
+                    75  => 1,
+                    79  => 1,
+                    86  => 1,
+                    90  => 1,
+                    98  => 1,
+                    102 => 1,
+                    117 => 1,
+                    121 => 1,
+                    125 => 1,
+                    144 => 1,
+                    157 => 1,
+                    161 => 1,
+                    167 => 1,
+                    188 => 1,
+                    190 => 1,
                 ];
 
-            case 'DeclareStatementsStyleUnitTest.4.inc':
+            case 'BlockModeUnitTest.5.inc':
                 return [
-                ];
-
-            case 'DeclareStatementsStyleUnitTest.5.inc':
-                return [
+                    51  => 1,
+                    55  => 1,
+                    56  => 1,
+                    66  => 1,
+                    86  => 1,
+                    111 => 1,
+                    117 => 1,
+                    126 => 1,
+                    134 => 1,
+                    135 => 1,
+                    144 => 1,
+                    148 => 1,
+                    172 => 1,
+                    181 => 1,
+                    185 => 1,
+                    192 => 1,
+                    213 => 1,
+                    229 => 1,
+                    239 => 1,
+                    245 => 1,
+                    248 => 1,
+                    268 => 1,
+                    276 => 1,
+                    281 => 1,
                 ];
 
             default:
