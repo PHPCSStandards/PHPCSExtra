b82be3e46879344342369cd2efa21c93c7f56564
jrfnl <jrfnl@users.noreply.github.com>
2022-12-03 06:35:38 +0100

    Universal/DisallowUse[Class|Const|Function]: minor code coverage tweak
    Ignore a statement which should never be reachable (defense in depth code).

--- Universal/Sniffs/UseStatements/DisallowUseClassSniff.php
+++ Universal/Sniffs/UseStatements/DisallowUseClassSniff.php
@@ -133,7 +133,7 @@ final class DisallowUseClassSniff implements Sniff
                 $reportPtr = $phpcsFile->findNext(\T_STRING, ($reportPtr + 1), $endOfStatement, false, $alias);
                 if ($reportPtr === false) {
                     // Shouldn't be possible.
-                    continue 2;
+                    continue 2; // @codeCoverageIgnore
                 }
 
                 $next = $phpcsFile->findNext(Tokens::$emptyTokens, ($reportPtr + 1), $endOfStatement, true);
