104aec2afaf9a5d92905c6bccd2884c3551fb3bf
jrfnl <jrfnl@users.noreply.github.com>
2023-05-21 01:27:00 +0200

    :sparkles: New `Universal.FunctionDeclarations.NoLongClosures` sniff
    New sniff to check for "long" closures and recommend using named functions instead.
    
    The sniff is fully configurable via the following properties:
    * (int) `recommendedLines` - this determines when a warning will be thrown. Defaults to `5`, which means that a warning will be thrown if the closure is more than 5 lines long.
    * (int) `maxLines` - this determines when an error will be thrown. Defaults to `8`, which means that an error will be thrown if the closure is more than 8 lines long.
    * (bool) `ignoreCommentLines` - whether or not comment-only lines should be ignored for the lines count. Defaults to `true`.
    * (bool) `ignoreEmptyLines` - whether or not blank lines should be ignored for the lines count. Defaults to `true`.
    
    The error and the warning have their own error codes  - `ExceedsMaximum` and `ExceedsRecommended` - and can be included/excluded separately from a ruleset.
    
    Includes unit tests.
    Includes documentation.
    Includes metrics.
    
    Closes 192

---
+++ Universal/Tests/FunctionDeclarations/NoLongClosuresUnitTest.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * PHPCSExtra, a collection of sniffs and standards for use with PHP_CodeSniffer.
+ *
+ * @package   PHPCSExtra
+ * @copyright 2020 PHPCSExtra Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSExtra
+ */
+
+namespace PHPCSExtra\Universal\Tests\FunctionDeclarations;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the NoLongClosures sniff.
+ *
+ * @covers PHPCSExtra\Universal\Sniffs\FunctionDeclarations\NoLongClosuresSniff
+ *
+ * @since 1.0.0
+ */
+final class NoLongClosuresUnitTest extends AbstractSniffUnitTest
+{
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * @param string $testFile The name of the file being tested.
+     *
+     * @return array <int line number> => <int number of errors>
+     */
+    public function getErrorList($testFile = '')
+    {
+        switch ($testFile) {
+            case 'NoLongClosuresUnitTest.1.inc':
+                return [
+                    102 => 1,
+                    105 => 1,
+                    108 => 1,
+                ];
+
+            case 'NoLongClosuresUnitTest.2.inc':
+                return [
+                    22 => 1,
+                    31 => 1,
+                    45 => 1,
+                    57 => 1,
+                ];
+
+            case 'NoLongClosuresUnitTest.3.inc':
+            case 'NoLongClosuresUnitTest.13.inc':
+            case 'NoLongClosuresUnitTest.17.inc':
+                return [
+                    57 => 1,
+                ];
+
+            case 'NoLongClosuresUnitTest.5.inc':
+            case 'NoLongClosuresUnitTest.6.inc':
+            case 'NoLongClosuresUnitTest.10.inc':
+            case 'NoLongClosuresUnitTest.11.inc':
+            case 'NoLongClosuresUnitTest.14.inc':
+            case 'NoLongClosuresUnitTest.16.inc':
+            case 'NoLongClosuresUnitTest.18.inc':
+            case 'NoLongClosuresUnitTest.19.inc':
+            case 'NoLongClosuresUnitTest.20.inc':
+            case 'NoLongClosuresUnitTest.21.inc':
+                return [
+                    45 => 1,
+                    57 => 1,
+                ];
+
+            case 'NoLongClosuresUnitTest.7.inc':
+            case 'NoLongClosuresUnitTest.8.inc':
+            case 'NoLongClosuresUnitTest.9.inc':
+            case 'NoLongClosuresUnitTest.12.inc':
+            case 'NoLongClosuresUnitTest.15.inc':
+            default:
+                return [];
+        }
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * @param string $testFile The name of the file being tested.
+     *
+     * @return array <int line number> => <int number of warnings>
+     */
+    public function getWarningList($testFile = '')
+    {
+        switch ($testFile) {
+            case 'NoLongClosuresUnitTest.1.inc':
+                return [
+                    42 => 1,
+                    50 => 1,
+                    58 => 1,
+                    83 => 1,
+                    91 => 1,
+                ];
+
+            case 'NoLongClosuresUnitTest.2.inc':
+                return [
+                    11 => 1,
+                ];
+
+            case 'NoLongClosuresUnitTest.3.inc':
+            case 'NoLongClosuresUnitTest.17.inc':
+                return [
+                    45 => 1,
+                ];
+
+            case 'NoLongClosuresUnitTest.4.inc':
+                return [
+                    22 => 1,
+                    31 => 1,
+                    45 => 1,
+                    57 => 1,
+                ];
+
+            case 'NoLongClosuresUnitTest.6.inc':
+            case 'NoLongClosuresUnitTest.10.inc':
+            case 'NoLongClosuresUnitTest.11.inc':
+            case 'NoLongClosuresUnitTest.14.inc':
+            case 'NoLongClosuresUnitTest.16.inc':
+            case 'NoLongClosuresUnitTest.18.inc':
+            case 'NoLongClosuresUnitTest.19.inc':
+            case 'NoLongClosuresUnitTest.20.inc':
+            case 'NoLongClosuresUnitTest.21.inc':
+                return [
+                    22 => 1,
+                    31 => 1,
+                ];
+
+            case 'NoLongClosuresUnitTest.13.inc':
+                return [
+                    31 => 1,
+                    45 => 1,
+                ];
+
+            case 'NoLongClosuresUnitTest.5.inc':
+            case 'NoLongClosuresUnitTest.7.inc':
+            case 'NoLongClosuresUnitTest.8.inc':
+            case 'NoLongClosuresUnitTest.9.inc':
+            case 'NoLongClosuresUnitTest.12.inc':
+            case 'NoLongClosuresUnitTest.15.inc':
+            default:
+                return [];
+        }
+    }
+}
