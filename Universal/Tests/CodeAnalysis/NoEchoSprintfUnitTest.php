6ee98241cb45023d2981fad66a04af90d5c8ff93
jrfnl <jrfnl@users.noreply.github.com>
2023-06-18 05:17:06 +0200

    :sparkles: New `Universal.CodeAnalysis.NoEchoSprintf` sniff
    Detects use of the inefficient `echo [v]sprintf(...);` combi. Use `[v]printf()` instead.
    
    Refs:
     * https://www.php.net/manual/en/function.printf.php
     * https://www.php.net/manual/en/function.sprintf.php
     * https://www.php.net/manual/en/function.vprintf.php
     * https://www.php.net/manual/en/function.vsprintf.php
    
    Includes unit tests.
    Includes documentation.

---
+++ Universal/Tests/CodeAnalysis/NoEchoSprintfUnitTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * PHPCSExtra, a collection of sniffs and standards for use with PHP_CodeSniffer.
+ *
+ * @package   PHPCSExtra
+ * @copyright 2023 PHPCSExtra Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSExtra
+ */
+
+namespace PHPCSExtra\Universal\Tests\CodeAnalysis;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the NoEchoSprintf sniff.
+ *
+ * @covers PHPCSExtra\Universal\Sniffs\CodeAnalysis\NoEchoSprintfSniff
+ *
+ * @since 1.1.0
+ */
+final class NoEchoSprintfUnitTest extends AbstractSniffUnitTest
+{
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * @param string $testFile The name of the file being tested.
+     *
+     * @return array <int line number> => <int number of errors>
+     */
+    public function getErrorList($testFile = '')
+    {
+        switch ($testFile) {
+            case 'NoEchoSprintfUnitTest.1.inc':
+                return [
+                    19 => 1,
+                    20 => 1,
+                    26 => 1,
+                    28 => 1,
+                    30 => 1,
+                    31 => 1,
+                ];
+
+            default:
+                return [];
+        }
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * @return array <int line number> => <int number of warnings>
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
