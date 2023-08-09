2fea560cb157a82bb8c338493f94b8870ce36216
jrfnl <jrfnl@users.noreply.github.com>
2023-08-09 03:10:03 +0200

    Modernize/Dirname: rename test case file
    ... in anticipation of additional test case files being added.

--- Modernize/Tests/FunctionCalls/DirnameUnitTest.php
+++ Modernize/Tests/FunctionCalls/DirnameUnitTest.php
@@ -28,43 +28,51 @@ final class DirnameUnitTest extends AbstractSniffUnitTest
      * The key of the array should represent the line number and the value
      * should represent the number of errors that should occur on that line.
      *
+     * @param string $testFile The name of the file being tested.
+     *
      * @return array<int, int>
      */
-    public function getErrorList()
+    public function getErrorList($testFile = '')
     {
-        return [
-            50  => 1,
-            51  => 1,
-            53  => 1,
-            56  => 1,
-            61  => 1,
-            62  => 1,
-            63  => 1,
-            65  => 1,
-            66  => 1,
-            69  => 1,
-            70  => 1,
-            73  => 1,
-            79  => 3,
-            80  => 3,
-            82  => 1,
-            83  => 2,
-            86  => 1,
-            94  => 1,
-            99  => 1,
-            107 => 1,
-            108 => 1,
-            110 => 2,
-            113 => 3,
-            114 => 3,
-            120 => 4,
-            121 => 4,
-            124 => 4,
-            127 => 1,
-            128 => 1,
-            130 => 1,
-            131 => 1,
-        ];
+        switch ($testFile) {
+            case 'DirnameUnitTest.1.inc':
+                return [
+                    50  => 1,
+                    51  => 1,
+                    53  => 1,
+                    56  => 1,
+                    61  => 1,
+                    62  => 1,
+                    63  => 1,
+                    65  => 1,
+                    66  => 1,
+                    69  => 1,
+                    70  => 1,
+                    73  => 1,
+                    79  => 3,
+                    80  => 3,
+                    82  => 1,
+                    83  => 2,
+                    86  => 1,
+                    94  => 1,
+                    99  => 1,
+                    107 => 1,
+                    108 => 1,
+                    110 => 2,
+                    113 => 3,
+                    114 => 3,
+                    120 => 4,
+                    121 => 4,
+                    124 => 4,
+                    127 => 1,
+                    128 => 1,
+                    130 => 1,
+                    131 => 1,
+                ];
+
+            default:
+                return [];
+        }
     }
 
     /**
